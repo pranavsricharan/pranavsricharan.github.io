9c1ec125f11d39481fa3f447e720ce1e9d88cd4e
pranavsricharan <pranav.sricharan@gmail.com>
2016-11-21 19:04:40 +0530

    New Design

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-//document.onscroll=function(){fixHeader()};
-//window.addEventListener("pageshow",animateBg);
-document.getElementById("nav").addEventListener("mouseover",function(){$("#mainArea").addClass("blur");console.log("over")});
-document.getElementById("nav").addEventListener("mouseout",function(){$("#mainArea").removeClass("blur");});
 
+//window.addEventListener("pageshow",animateBg);
+document.getElementById("nav").addEventListener("mouseover",function(){$("#mainArea").addClass("blur");});
+document.getElementById("nav").addEventListener("mouseout",function(){$("#mainArea").removeClass("blur");});
+document.onscroll=function(){animateSkills()};
+function animateSkills()
+{
+	if(document.getElementById("stat").getBoundingClientRect().top < 450)
+		$(".statProgress").removeClass("noProgress");
+	
+	
+}
